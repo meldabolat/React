a90d6e3d673fb2b4315fe4b558bc74a2503d0ccd
melda bolat <121611832+meldabolat@users.noreply.github.com>
2024-08-20 10:58:28 +0300

    Hooks

--- React-calisma/src/App.jsx
+++ React-calisma/src/App.jsx
@@ -1,32 +1,16 @@
 
+import { useEffect, useState } from 'react';
 import './App.css'
+import Hooks from './Hooks';
 
 function App() {
-  //JS kodları yazılır
-let a = 15;
-const firstName ="melda"
-let vize1 = 80
-let vize2 = 60
-let sonuc = true
-let isimler = ["melda","irem","yiğit","sefa"]
-  return (
-    //HTML kodları yazılır
-    //jsx : {}
-  <div>
-<p> a değişkeninin değeri : {a} </p>
-<p>müşterinin adı {firstName}</p>
-<p>ortalama = {(vize1+vize2)/2}</p>
-  {/*  ternary operator */ }
-{sonuc ? <p>ehliyeti alabilirsin</p> : <p>ehliyeti alamazsın</p>} 
 
-{(vize1+vize2)/2 >=50 ? <p>geçtin</p> : <p>kaldın</p> }
-{
-  isimler.map((isim, index)=>(
-    <div style={{backgroundColor:'orange', border: '1px solid black'}} key={index}> {isim} </div>
-))
-}
-  </div>
-  )
+  return (
+    <div>
+      <Hooks />
+    </div>
+ 
+  );
 }
 
 export default App
