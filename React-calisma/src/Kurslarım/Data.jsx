273dd99cbe91b1d1c5d786c72c9271ace67bce74
melda bolat <121611832+meldabolat@users.noreply.github.com>
2024-08-21 11:42:16 +0300

    Kurs projesi

---
+++ React-calisma/src/Kurslarım/Data.jsx
@@ -0,0 +1,38 @@
+import csharp from '../assets/hq720.jpg'
+import devex from '../assets/hq720 (1).jpg'
+import figma from '../assets/figma.jpg'
+import mvc from '../assets/hqdefault.jpg'
+export const courses = [
+    {
+        id: 1244,
+        title: "C # programlama dili",
+        description :" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, repellendus?, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui, totam!",
+        price : 120,
+        img : csharp,
+
+    },
+    {
+        id: 1255,
+        title: "Devex programlama dili",
+        description :" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, repellendus?, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui, totam!",
+        price : 130,
+        img : devex,
+
+    },
+    {
+        id: 1266,
+        title: "Figma ",
+        description :" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, repellendus?, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui, totam!",
+        price : 100,
+        img : figma,
+
+    },
+    {
+        id: 1277,
+        title: "MVC5 programlama dili",
+        description :" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, repellendus?, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui, totam!",
+        price : 180,
+        img : mvc,
+
+    }
+]
